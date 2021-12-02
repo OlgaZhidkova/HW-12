b06fe568d85f85be44f8c400f655cadfaa5f1934
Olga Zhidkova <zhidkova.olga12@gmail.com>
2021-12-03 00:02:04 +0300

    Add TableView, DataSourse, Delegate.

--- HW 12/ViewController.swift
+++ HW 12/ViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    
+    let tableView = UITableView(frame: .zero, style: .grouped)
+    var settings = [SettingOptions]()
     
     // MARK: - Lifecycle
     
@@ -18,6 +19,11 @@ class ViewController: UIViewController {
         setupView()
         setupHierarchy()
         setupLayout()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
     }
 
@@ -31,12 +37,43 @@ class ViewController: UIViewController {
     }
     
     private func setupHierarchy() {
+        view.addSubview(tableView)
     
     }
     
     private func setupLayout() {
-        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
+    
+    // MARK: - Functions
+    
+    
+    
+    // MARK: - DataSourse methods
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let setting = settings[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = setting.title
+        return cell
+    }
+    
+    // MARK: - Delegate methods
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
 }
 
+struct SettingOptions {
+    let title: String?
+    let icon: UIImage?
+}
