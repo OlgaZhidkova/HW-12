0787f79890f6e8dfc8499844b9ef8d6ea91bdff3
Olga Zhidkova <zhidkova.olga12@gmail.com>
2021-12-07 16:48:50 +0300

    Add class SwitchTableViewCell.

--- HW 12/ViewController.swift
+++ HW 12/ViewController.swift
@@ -7,10 +7,33 @@
 
 import UIKit
 
+struct Section {
+    let title: String
+    let options: [SettingType]
+}
+
+enum SettingType {
+    case staticCell(type: SettingOptions)
+    case switchCell(type: SettingSwitchOptions)
+}
+
+struct SettingOptions {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+}
+
+struct SettingSwitchOptions {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    var isOn: Bool
+}
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let tableView = UITableView(frame: .zero, style: .grouped)
-    var settings = [SettingOptions]()
+    var settings = [Section]()
     
     // MARK: - Lifecycle
     
@@ -25,14 +48,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         
         tableView.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifire)
+        tableView.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifire)
     }
 
     // MARK: - Settings
     
     private func setupView() {
+        view.backgroundColor = .systemGray6
         navigationItem.title = "Настройки"
-        navigationController?.navigationBar.barTintColor = .lightGray
-        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     private func setupHierarchy() {
@@ -50,32 +74,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // MARK: - Functions
     
     func configure() {
-        self.settings = Array(1...100).compactMap({
-            SettingOptions(title: "Item \($0)", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemGray)
-        })
+        settings.append(Section(title: "First", options: [
+            
+            .switchCell(type: SettingSwitchOptions(title: "Авиарежим", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemOrange, isOn: true)),
+            .staticCell(type:SettingOptions(title: "Wi-Fi", icon: UIImage(systemName: "wifi"), iconBackgroundColor: .systemBlue))
+        ]))
     }
     
     // MARK: - DataSourse methods
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return settings.count
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings[section].options.count
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let setting = settings[indexPath.row]
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifire, for: indexPath) as? SettingTableViewCell else {
-            return UITableViewCell()
+        let setting = settings[indexPath.section].options[indexPath.row]
+        
+        switch setting.self {
+        case .staticCell(let setting):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifire, for: indexPath) as? SettingTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.accessoryType = .disclosureIndicator
+            cell.configure(with: setting)
+            return cell
+        case .switchCell(let setting):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SwitchTableViewCell.identifire, for: indexPath) as? SwitchTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.accessoryType = .none
+            cell.selectionStyle = .none
+            cell.configure(with: setting)
+            return cell
         }
-        cell.configure(with: setting)
-        return cell
     }
     
     // MARK: - Delegate methods
     
-}
-
-struct SettingOptions {
-    let title: String?
-    let icon: UIImage?
-    let iconBackgroundColor: UIColor
 }
